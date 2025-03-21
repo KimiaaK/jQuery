c1a8680f40ba072a4be609b2c1d95c07dc2fc624
Kimia Ketabforoosh <keam96@ymail.com>
2025-03-21 16:25:55 -0400

    'practices and comments'

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-$("h1").css("color","red");
+//does exactly the same thing as writing the <script> at the end of
+//the body in the index.html file
+//$(document).ready(function(){
+//    $("h1").css("color","red");  
+//})
+
+
+//not advised as index.js is fpr behaviour and not appearrance
+//console.log($('h1').css('font-size')); //gives you the value of h1
+//$('h1').css('font-size', '5rem'); //set the value
+
